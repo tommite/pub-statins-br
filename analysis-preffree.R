203169724baacf68a1233ea28e4d07f6667204e9
Tommi Tervonen <tervonen@ese.eur.nl>
2014-07-24 14:16:20 +0200

    Change plots

--- analysis-preffree.R
+++ analysis-preffree.R
@@ -6,12 +6,12 @@ result.pref.free <- smaa(part.values, w.W)
 
 ## Plot rank acceptabilities
 pdf('ra-preffree.pdf')
-plot(result.pref.free$ra)
+plot(result.pref.free$ra, main='')
 dev.off()
 
 ## Plot central weights for alts >= min.cf.limit
 pdf('cw-preffree.pdf')
-plot(NA, xlim=c(1, length(outcomes)), ylim=c(0, 0.30), xlab="", ylab="Weight", xaxt='n')
+plot(NA, xlim=c(1, length(outcomes)), ylim=c(0, 0.30), xlab="", ylab="Weight", xaxt='n', main='')
 axis(side=1, at=1:7, labels=outcomes, las=2)
 result.pref.free.cw <- smaa.cf(part.values, result.pref.free$cw)
 pfree.to.plot <- result.pref.free.cw$cf >= min.cf.limit
