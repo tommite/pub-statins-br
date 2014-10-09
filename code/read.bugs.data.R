a2474acbffeade2198bea0dfda866f252922b9bd
Tommi Tervonen <tommi@damirv-vps.colo.transip.net>
2014-10-09 09:41:48 +0200

    Add 'only.included' to read.bugs.data

--- code/read.bugs.data.R
+++ code/read.bugs.data.R
@@ -1,15 +1,17 @@
 
 ### Convert data to one-row-per-arm format
-read.bugs.data <- function(file, filter=c('m', 'p', 's')) {
+read.bugs.data <- function(file, filter=c('m', 'p', 's'), only.included=FALSE) {
 	data <- read.table(file, header=T)
-        pops <- data$population
+        pops <- data$popind
+	incs <- data$include
 
 	n <- length(grep('^r\\.\\.', colnames(data)))
 	cols <- c('r', 'n', 't')
 	data <- do.call(rbind, lapply(1:n, function(i) {
 		df <- do.call(data.frame,
 			c(lapply(cols, function(colname) {
-				data[[paste(colname, '..', i, '.', sep='')]]
+				data[[paste(colname, 
+					'..', i, '.', sep='')]]
 			}), list(1:length(data$na)))
 		)
 		colnames(df) <- c(cols, 's')
@@ -19,6 +21,12 @@ read.bugs.data <- function(file, filter=c('m', 'p', 's')) {
 	data$n <- as.integer(data$n)
 	data <- data[!is.na(data$t),]
         data$pop <- pops[data$s] # add population identifier
+        data$inc <- incs[data$s] # add inclusion identifier
         # filter out studies not matching the filter
-	data[data$pop %in% filter,]
+	pop.filtered <- data[data$pop %in% filter,]
+	# if only.included, filter those
+	if (only.included) {
+	   pop.filtered <- pop.filtered[pop.filtered$inc == 'y',]
+	}
+	pop.filtered[,colnames(pop.filtered) != 'inc']
 }
