5caacfe8ae409c7034293785b360d22d48a8b371
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2014-10-30 15:00:48 -0400

    Plot risk differences

--- figs/quantile-fig.R
+++ figs/quantile-fig.R
@@ -5,27 +5,27 @@ source('params.R')
 meas <- lapply(outcomes, function(outcome) { gen.meas(dget(paste('data/', outcome, '.p.meas.txt', sep=''))) })
 names(meas) <- outcomes
 
-meas.rel <- lapply(outcomes, function(outcome) { gen.meas(dget(paste('data/', outcome, '.p.meas.txt', sep='')), baseline.uncertainty=FALSE) })
-names(meas.rel) <- outcomes
-
 calc.quantiles <- function(meas) {
 	lapply(meas, oc.quantiles)
 }
 
 quants <- calc.quantiles(meas)
 
+meas.rel <- lapply(meas, function(x) {
+  x[, colnames(x) != "Control"] - x[, "Control"]
+})
 quants.rel <- calc.quantiles(meas.rel)
 
-plotQuantiles <- function(q.oc, outcome, xlim=c(0, round(max(quants[[outcome]]), 2)+0.01)) {
+plotQuantiles <- function(q.oc, outcome, scale, xlim=c(0, round(max(quants[[outcome]]), 2)+0.01)) {
   data <- data.frame(id=colnames(q.oc), pe=q.oc['50%',],
-                     ci.l=q.oc['2.5%',], ci.u=q.oc['97.5%',], style='normal')
+                     ci.l=q.oc['2.5%',], ci.u=q.oc['97.5%',], style='pooled')
   blobbogram(data, xlim=xlim,
-             id.label="Treatment", ci.label="Risk (95% CI)", right.label=outcome)
+             id.label="Treatment", ci.label=paste(scale, "(95% CI)"), right.label=outcome)
 }
 
 ## make quantile figs
 for (oc in outcomes) {
     pdf(paste('figs/quantiles-', oc, '.pdf', sep=''))
-    plotQuantiles(quants[[oc]], oc)
+    plotQuantiles(quants.rel[[oc]], oc, scale="Risk difference", xlim=c(-0.1,0.1))
     dev.off()
 }
